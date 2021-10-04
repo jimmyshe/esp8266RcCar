27a1b6d517c2592b616048c3b530797c9de997e8
SizhenShe <cq2essz@gmail.com>
2021-10-04 22:36:34 +0800

    use udp instead of mqtt

--- src/main.cpp
+++ src/main.cpp
@@ -2,12 +2,16 @@
 #include <ArduinoJson.h>
 #include <ESP8266WiFi.h>
 #include <ESP8266mDNS.h>
-#include <PubSubClient.h>
 #include <Servo.h>
 #include <WiFiManager.h>
 #include <ctime>
+#include <memory>
 #include <sstream>
 #include <string>
+
+#include "ESPAsyncUDP.h"
+
+AsyncUDP udp;
 
 //输入输出针脚
 constexpr uint8_t FORWARD_SPEED_CHANNEL = D6;
@@ -20,26 +24,24 @@ ADC_MODE(ADC_VCC);
 void initializePin();
 
 // global variables
-std::unique_ptr<TickerScheduler> ts;
+TickerScheduler ts(2);
 std::unique_ptr<WiFiManager> wifiManager;
-std::unique_ptr<WiFiClient> wifiClient;
-std::unique_ptr<PubSubClient> mqttClient;
-std::unique_ptr<Servo> steeringServo;
+Servo steeringServo;
 unsigned long controlMsgUpdateTime = 0;
 
-void heartbeat(void *argc) {
-  static unsigned int beat{0};
-  if (mqttClient->connected()) {
-    DynamicJsonDocument doc(200);
-    doc["beat"] = beat;
-    doc["vcc"] = EspClass::getVcc();
-    doc["uptime"] = millis();
-    std::string ss;
-    serializeJson(doc, ss);
-    mqttClient->publish("esp8266", ss.c_str());
-    ++beat;
-  }
-}
+// void heartbeat(void *argc) {
+//   static unsigned int beat{0};
+//   if (mqttClient->connected()) {
+//     DynamicJsonDocument doc(200);
+//     doc["beat"] = beat;
+//     doc["vcc"] = EspClass::getVcc();
+//     doc["uptime"] = millis();
+//     std::string ss;
+//     serializeJson(doc, ss);
+//     mqttClient->publish("esp8266", ss.c_str());
+//     ++beat;
+//   }
+// }
 
 void WatchDog(void *argc) {
   // controlMsgUpdateTime
@@ -49,13 +51,31 @@ void WatchDog(void *argc) {
   }
 }
 
-void MsgCallback(char *topic, byte *payload, unsigned int length) {
-  //    auto s = std::string((char *)payload, length);
-  //    Serial.printf("Message arrived [%s] \n \t %s", topic, s.c_str());
-  //    Serial.println();
+void MsgCallback(AsyncUDPPacket packet) {
+
+//  Serial.print("UDP Packet Type: ");
+//  Serial.print(packet.isBroadcast()   ? "Broadcast"
+//               : packet.isMulticast() ? "Multicast"
+//                                      : "Unicast");
+//  Serial.print(", From: ");
+//  Serial.print(packet.remoteIP());
+//  Serial.print(":");
+//  Serial.print(packet.remotePort());
+//  Serial.print(", To: ");
+//  Serial.print(packet.localIP());
+//  Serial.print(":");
+//  Serial.print(packet.localPort());
+//  Serial.print(", Length: ");
+//  Serial.print(packet.length());
+//  Serial.print(", Data: ");
+//  Serial.write(packet.data(), packet.length());
+//  Serial.println();
+//  // reply to the client
+//  packet.printf("Got %u bytes of data", packet.length());
+
   static double oldThrottle = 0;
   static DynamicJsonDocument doc(1000);
-  auto error = deserializeJson(doc, payload);
+  auto error = deserializeJson(doc, packet.data());
   if (error) {
     Serial.printf("Error deserializing %s", error.c_str());
     Serial.println("msg invalid");
@@ -74,7 +94,7 @@ void MsgCallback(char *topic, byte *payload, unsigned int length) {
     Serial.printf("%f %f %f", throttle, brake, steeringAngle);
     Serial.println("");
 
-    steeringServo->write(steeringAngle);
+    steeringServo.write(steeringAngle);
     digitalWrite(MOTOR_ENABLE_CHANNEL, HIGH);
 
     if (brake > 0) {
@@ -88,13 +108,13 @@ void MsgCallback(char *topic, byte *payload, unsigned int length) {
         analogWrite(MOTOR_ENABLE_CHANNEL, PwmRange - 1);
 
         int throttlePWM = throttle * PwmRange;
-        throttlePWM = throttlePWM / 2;
+//        throttlePWM = throttlePWM / 2;
 
         if (not reverse) {
           analogWrite(FORWARD_SPEED_CHANNEL, throttlePWM);
           analogWrite(BACKWARD_SPEED_CHANNEL, 0);
         } else {
-          throttlePWM = throttlePWM / 2;
+          throttlePWM = throttlePWM / 4;
           analogWrite(FORWARD_SPEED_CHANNEL, 0);
           analogWrite(BACKWARD_SPEED_CHANNEL, throttlePWM);
         }
@@ -107,33 +127,6 @@ void MsgCallback(char *topic, byte *payload, unsigned int length) {
   }
 }
 
-void reconnect() {
-  static time_t lastConnectedTimeStamp = 0;
-  constexpr int waitForConnectionTime = 5;
-  auto now = std::time(nullptr);
-  auto diff = now - lastConnectedTimeStamp;
-  // Loop until we're reconnected
-  if (!mqttClient->connected() and diff > waitForConnectionTime) {
-    Serial.print("Attempting MQTT connection...");
-    // Create a random client ID
-    String clientId = "ESP8266Client-";
-    clientId += String(EspClass::random(), HEX);
-    // Attempt to connect
-    if (mqttClient->connect(clientId.c_str(), "cisai", "cisai123456")) {
-      Serial.println("connected");
-      // ... and resubscribe
-      mqttClient->subscribe("RcControl");
-      lastConnectedTimeStamp = std::time(nullptr);
-    } else {
-      Serial.print("failed, rc=");
-      Serial.print(mqttClient->state());
-      Serial.printf(" try again in %d seconds", waitForConnectionTime);
-      Serial.println();
-      lastConnectedTimeStamp = std::time(nullptr);
-    }
-  }
-}
-
 void setup(void) {
   initializePin();
 
@@ -142,12 +135,7 @@ void setup(void) {
 
   Serial.begin(115200);
 
-  ts = std::make_unique<TickerScheduler>(2);
   wifiManager = std::make_unique<WiFiManager>();
-  wifiClient = std::make_unique<WiFiClient>();
-  mqttClient = std::make_unique<PubSubClient>(*wifiClient);
-  mqttClient->setServer(IPAddress(192, 168, 0, 2), 1883);
-  mqttClient->setCallback(MsgCallback);
 
   auto res = wifiManager->autoConnect("my8266", "67871218");
   if (!res) {
@@ -164,8 +152,7 @@ void setup(void) {
     Serial.print(".");
   }
 
-  ts->add(0, 2000, heartbeat, nullptr, true);
-  ts->add(1, 200, WatchDog, nullptr, true);
+  ts.add(1, 200, WatchDog, nullptr, true);
 
   Serial.println("");
   Serial.print("Connected to ");
@@ -173,20 +160,18 @@ void setup(void) {
   Serial.print("IP address: ");
   Serial.println(WiFi.localIP());
 
-  reconnect();
+  if (udp.listen(8080)) {
+    Serial.print("UDP Listening on IP: ");
+    Serial.println(WiFi.localIP());
+    udp.onPacket([](const AsyncUDPPacket &packet) { MsgCallback(packet); });
+  }
 }
 void initializePin() {
-  steeringServo = std::make_unique<Servo>();
-  steeringServo->attach(STEER_CHANNEL);
+  Serial.println("initialize Pin");
+  steeringServo.attach(STEER_CHANNEL);
   pinMode(FORWARD_SPEED_CHANNEL, OUTPUT);
   pinMode(BACKWARD_SPEED_CHANNEL, OUTPUT);
   pinMode(MOTOR_ENABLE_CHANNEL, OUTPUT);
 }
 
-void loop(void) {
-  ts->update();
-  if (!mqttClient->connected()) {
-    reconnect();
-  }
-  mqttClient->loop();
-}
+void loop(void) { ts.update(); }
